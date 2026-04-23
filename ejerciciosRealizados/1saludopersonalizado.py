65c2e5ffff710cc755f564d9d6914eab300692d5
meacostausistemas-blip <meacosta_usistemas@intep.edu.co>
2026-04-23 15:34:36 -0500

    Add files via upload

---
+++ ejerciciosRealizados/1saludopersonalizado.py
@@ -0,0 +1,6 @@
+# 1. El saludo personalizado
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuál es tu edad? "))
+añio_actual = 2026
+añio_nacimiento = añio_actual - edad
+print(f"Bienvenido {nombre}, naciste aproximadamente en {añio_nacimiento}.")
---
+++ ejerciciosRealizados/2paroimpar.py
@@ -0,0 +1,6 @@
+# 2. Par o Impar
+num = int(input("Ingresa un número entero: "))
+if num % 2 == 0:
+    print("Es par")
+else:
+    print("Es impar")
